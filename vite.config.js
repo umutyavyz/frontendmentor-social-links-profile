ce9036ecd78aef8dbb75a5aee272ebe930df468a
Umut Yavuz <umtyavuz@icloud.com>
2025-07-25 19:38:24 +0300

    GitHub Pages Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/frontendmentor-social-links-profile/",
   plugins: [react(), tailwindcss()],
 })
